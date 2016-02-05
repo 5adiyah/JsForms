81e8c5e008eb23fbfbf7863bf3c47633326ee42c
5adiyah <sadiali@pdx.edu>
2016-02-05 01:29:37 -0800

    Yeh forgot to commit along the way again

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-$(document).ready(function(){
-  $(".button#hello").click(function(){
-    $("ul").prepend("<li>Hello!</li>");
-  });
+$(document).ready(function() {
+  $('#blanks form').submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var positionInput = $("input#position").val();
+    var animalInput = $("input#animal").val();
+    var crimeInput = $("input#crime").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+    var fluidInput = $("input#fluid").val();
+    var numberInput = $("input#number").val();
+    var animal2Input = $("input#animal2").val();
 
-  $(".button#goodbye").click(function(){
-    $("ul").prepend("<li>Goodbye!</li>");
-  });
+    $('.person1').text(person1Input);
+    $('.animal').text(animalInput);
+    $('.crime').text(crimeInput);
+    $('.verb').text(verbInput);
+    $('.noun').text(nounInput);
+    $('.fluid').text(fluidInput);
+    $('.number').text(numberInput);
+    $('.animal2').text(animal2Input);
 
-  $(".button#stop").click(function(){
-    $("ul").prepend("<li>Stop Copying Me!</li>");
+    $("#story").show();
+
+    event.preventDefault();
   });
 
 });
